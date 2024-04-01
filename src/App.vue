09446acc6f0aa9018192f7901e7884f564874461
junhyok7146 <junhyeok_an@naver.com>
2024-04-01 16:50:37 +0900

    0401 로딩스피너

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
+    <div class="loading-overlay" v-show="isLoading">
+      <div class="spinner"></div>
+      <iframe src="https://lottie.host/embed/5ea2414a-5bde-4f71-bc5b-7c724bd55c09/fKltP5yiqk.json"></iframe>
+      <p>Loading...</p>
+    </div>
+    <div v-show="!isLoading">
     <Header />
     <router-view />
     <Footer />
+      <test-section />
+      <event-section />
+      <best-product-section />
+      <slide-section2 />
+      <hot-key-word-section />
+      <new-prd-section />
+      <instargram-section />
+      <go-link-section />
+    </div>
   </div>
 </template>
 
@@ -16,9 +31,17 @@ export default {
     Header,
     Footer,
   },
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
   mounted() {
     this.updateScreenWidth();
     window.addEventListener("resize", this.updateScreenWidth);
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
   },
   beforeDestroy() {
     window.removeEventListener("resize", this.updateScreenWidth);
@@ -40,4 +63,24 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.spinner {
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #3498db;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 2s linear infinite;
+}
+</style>
---
+++ src/views/modal/Modal.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <section-titke title="결제하기" />
+        <div>modal</div>
+        <div>overlay</div>
+        <div>moadl-card</div>
+    </div>
+</template>
+
+<script>
+    import SectionTitle from "@/components/layout/SectionTitle.vue"
+    export default {
+        compomemt:{
+        SectionTitle,
+    }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
